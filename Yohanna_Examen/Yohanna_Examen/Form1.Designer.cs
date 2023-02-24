67c092ae4b447e714f3c7274cccc4f377966ffff
yohanna <yohannavilchez02@gmail.com>
2023-02-24 00:23:55 +0000

    Form1_Terminado
    Completo

--- Yohanna_Examen/Yohanna_Examen/Form1.Designer.cs
+++ Yohanna_Examen/Yohanna_Examen/Form1.Designer.cs
@@ -32,21 +32,31 @@ namespace Yohanna_Examen
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.bt_calcular = new System.Windows.Forms.Button();
             this.Mostrar_Interes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 78);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(412, 229);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.Size = new System.Drawing.Size(150, 35);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "L. 200,000.00";
             // 
             // bt_calcular
             // 
-            this.bt_calcular.Location = new System.Drawing.Point(361, 78);
+            this.bt_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_calcular.ForeColor = System.Drawing.Color.Maroon;
+            this.bt_calcular.Location = new System.Drawing.Point(323, 426);
             this.bt_calcular.Name = "bt_calcular";
-            this.bt_calcular.Size = new System.Drawing.Size(119, 76);
+            this.bt_calcular.Size = new System.Drawing.Size(119, 36);
             this.bt_calcular.TabIndex = 1;
             this.bt_calcular.Text = "CALCULAR";
             this.bt_calcular.UseVisualStyleBackColor = true;
@@ -56,16 +66,97 @@ namespace Yohanna_Examen
             // 
             this.Mostrar_Interes.FormattingEnabled = true;
             this.Mostrar_Interes.ItemHeight = 20;
-            this.Mostrar_Interes.Location = new System.Drawing.Point(12, 170);
+            this.Mostrar_Interes.Location = new System.Drawing.Point(10, 488);
             this.Mostrar_Interes.Name = "Mostrar_Interes";
-            this.Mostrar_Interes.Size = new System.Drawing.Size(776, 264);
+            this.Mostrar_Interes.Size = new System.Drawing.Size(776, 304);
             this.Mostrar_Interes.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 56);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BANCO VILCHEZ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(685, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Gracias por su preferencia y confianza ha nuestro servicio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(779, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "_____________________________________________________________________________";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(194, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(377, 37);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CALCULO DE INTERESES";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(291, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Su abono mensual es de: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(526, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "El interes Brindado mensualmente es de:  1.5% ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Location = new System.Drawing.Point(50, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(655, 29);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Interes Ganado MENSUALMENTE  durante el año 2023";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 802);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Mostrar_Interes);
             this.Controls.Add(this.bt_calcular);
             this.Controls.Add(this.textBox1);
@@ -81,6 +172,13 @@ namespace Yohanna_Examen
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button bt_calcular;
         private System.Windows.Forms.ListBox Mostrar_Interes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
