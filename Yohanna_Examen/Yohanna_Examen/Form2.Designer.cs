01bb7fc667abf0baf91186aa8c957c9db3b18be9
yohanna <yohannavilchez02@gmail.com>
2023-02-24 01:14:44 +0000

    Formulario2
    Formulario2 terminado

---
+++ Yohanna_Examen/Yohanna_Examen/Form2.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Yohanna_Examen
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_producto = new System.Windows.Forms.TextBox();
+            this.tb_precio = new System.Windows.Forms.TextBox();
+            this.tb_cantidad = new System.Windows.Forms.TextBox();
+            this.bt_calcular = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tb_sub = new System.Windows.Forms.TextBox();
+            this.tb_des = new System.Windows.Forms.TextBox();
+            this.tb_total = new System.Windows.Forms.TextBox();
+            this.bt_limpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MERCADITO VILCHEZ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(5, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(403, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "==========================";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producto: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cantidad:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(234, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "FACTURA 25/02/2023  2:00 pm";
+            // 
+            // tb_producto
+            // 
+            this.tb_producto.Location = new System.Drawing.Point(278, 135);
+            this.tb_producto.Name = "tb_producto";
+            this.tb_producto.Size = new System.Drawing.Size(130, 26);
+            this.tb_producto.TabIndex = 6;
+            // 
+            // tb_precio
+            // 
+            this.tb_precio.Location = new System.Drawing.Point(278, 184);
+            this.tb_precio.Name = "tb_precio";
+            this.tb_precio.Size = new System.Drawing.Size(130, 26);
+            this.tb_precio.TabIndex = 7;
+            // 
+            // tb_cantidad
+            // 
+            this.tb_cantidad.Location = new System.Drawing.Point(278, 231);
+            this.tb_cantidad.Name = "tb_cantidad";
+            this.tb_cantidad.Size = new System.Drawing.Size(130, 26);
+            this.tb_cantidad.TabIndex = 8;
+            // 
+            // bt_calcular
+            // 
+            this.bt_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_calcular.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.bt_calcular.Location = new System.Drawing.Point(145, 305);
+            this.bt_calcular.Name = "bt_calcular";
+            this.bt_calcular.Size = new System.Drawing.Size(127, 31);
+            this.bt_calcular.TabIndex = 9;
+            this.bt_calcular.Text = "CALCULAR";
+            this.bt_calcular.UseVisualStyleBackColor = true;
+            this.bt_calcular.Click += new System.EventHandler(this.bt_calcular_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(15, 382);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 29);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "SubTotal: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(15, 434);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(234, 29);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Descuento del 15%: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(20, 492);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 29);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "TOTAL:";
+            // 
+            // tb_sub
+            // 
+            this.tb_sub.Location = new System.Drawing.Point(278, 386);
+            this.tb_sub.Name = "tb_sub";
+            this.tb_sub.Size = new System.Drawing.Size(128, 26);
+            this.tb_sub.TabIndex = 13;
+            // 
+            // tb_des
+            // 
+            this.tb_des.Location = new System.Drawing.Point(278, 438);
+            this.tb_des.Name = "tb_des";
+            this.tb_des.Size = new System.Drawing.Size(127, 26);
+            this.tb_des.TabIndex = 14;
+            // 
+            // tb_total
+            // 
+            this.tb_total.Location = new System.Drawing.Point(278, 496);
+            this.tb_total.Name = "tb_total";
+            this.tb_total.Size = new System.Drawing.Size(127, 26);
+            this.tb_total.TabIndex = 15;
+            // 
+            // bt_limpiar
+            // 
+            this.bt_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_limpiar.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.bt_limpiar.Location = new System.Drawing.Point(145, 549);
+            this.bt_limpiar.Name = "bt_limpiar";
+            this.bt_limpiar.Size = new System.Drawing.Size(116, 31);
+            this.bt_limpiar.TabIndex = 16;
+            this.bt_limpiar.Text = "LIMPIAR";
+            this.bt_limpiar.UseVisualStyleBackColor = true;
+            this.bt_limpiar.Click += new System.EventHandler(this.bt_limpiar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 604);
+            this.Controls.Add(this.bt_limpiar);
+            this.Controls.Add(this.tb_total);
+            this.Controls.Add(this.tb_des);
+            this.Controls.Add(this.tb_sub);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.bt_calcular);
+            this.Controls.Add(this.tb_cantidad);
+            this.Controls.Add(this.tb_precio);
+            this.Controls.Add(this.tb_producto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_producto;
+        private System.Windows.Forms.TextBox tb_precio;
+        private System.Windows.Forms.TextBox tb_cantidad;
+        private System.Windows.Forms.Button bt_calcular;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tb_sub;
+        private System.Windows.Forms.TextBox tb_des;
+        private System.Windows.Forms.TextBox tb_total;
+        private System.Windows.Forms.Button bt_limpiar;
+    }
+}
---
+++ Yohanna_Examen/Yohanna_Examen/Form2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yohanna_Examen
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private async void bt_calcular_Click(object sender, EventArgs e)
+        {
+            int cantidad = Convert.ToInt32(tb_cantidad.Text);
+            int precio = Convert.ToInt32(tb_precio.Text);
+
+            tb_sub.Text = (" " + await SubTotalAsync(cantidad, precio));
+            tb_des.Text = (" " + await DescuentoAsync(cantidad, precio));
+            tb_total.Text = (" " + await TotalPagarAsync(cantidad, precio));
+        }
+        //=================================================================
+        public int SubTotal(int cantidad, int precio)
+        {
+            return cantidad * precio;
+        }
+
+        public async Task<int> SubTotalAsync(int cantidad, int precio)
+        {
+            var SubTotal = await Task.Run(() => {
+                return cantidad * precio;
+            });
+            return SubTotal;
+        }
+        //==================================================================
+        public double Descuento(int cantidad, int precio)
+        {
+            return (cantidad * precio) * 0.15;
+        }
+       
+        public async Task<double> DescuentoAsync(int cantidad, int precio)
+        {
+            var Descuento = await Task.Run(() => {
+                return (cantidad * precio) * 0.15;
+            });
+            return Descuento;
+        }
+
+        //===================================================================
+        public double TotalPagar(int cantidad, int precio)
+        {
+            return ((cantidad * precio) - (cantidad * precio) * 0.15);
+        }
+        public async Task<double> TotalPagarAsync(int cantidad, int precio)
+        {
+            var TotalPagar = await Task.Run(() => {
+                return ((cantidad * precio) - (cantidad * precio) * 0.15);
+            });
+            return TotalPagar;
+        }
+        //===================================================================
+        public void Limpiar()
+        {
+            tb_sub.Text = " ";
+            tb_des.Text = " ";
+            tb_total.Text = " ";
+            tb_producto.Text = " ";
+            tb_precio.Text = " ";
+            tb_cantidad.Text = " ";
+        }
+
+        private void bt_limpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
